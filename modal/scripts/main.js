c4614a96c83c591e696efd04ccc4d8007abf8e45
Brandon <brandondong604@hotmail.com>
2019-01-08 23:32:27 -0800

    Image selection styling

---
+++ modal/scripts/main.js
@@ -0,0 +1,19 @@
+function main() {
+  const submitButton = document.getElementById("submit");
+  const toggleLink = document.getElementById("toggle");
+  const imageContainers = document.getElementsByClassName("image-container");
+  
+  submitButton.addEventListener("click", () => {
+    alert("hi");
+  });
+  toggleLink.addEventListener("click", () => {
+    alert("hola");
+  });
+  for (const ic of imageContainers) {
+    ic.addEventListener("click", () => {
+      alert("img");
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", main);
