7a458f122613beb14964ace44e8f5651a2632c19
Brandon <brandondong604@hotmail.com>
2019-01-11 01:02:26 -0800

    Add copy and paste logic

--- modal/scripts/captcha-manager.js
+++ modal/scripts/captcha-manager.js
@@ -1,17 +1,26 @@
 export default class CaptchaManager {
   constructor() {
-    this.numQuestionsAnswered = 0;
+    this.isImageMode = true;
+    this.numImageQuestionsAnswered = -1;
+    this.numAudioQuestionsAnswered = -1;
   }
   
   nextQuestion() {
-    this.numQuestionsAnswered++;
+    if (this.isImageMode) {
+      this.numImageQuestionsAnswered++;
+    } else {
+      this.numAudioQuestionsAnswered++;
+    }
   }
   
   canSwithQuestionTypes() {
-    return this.numQuestionsAnswered >= 5;
+    if (this.isImageMode) {
+      return this.numImageQuestionsAnswered >= 5;
+    }
+    return this.numAudioQuestionsAnswered >= 5;
   }
   
   switchQuestionTypes() {
-    alert("Switching");
+    this.isImageMode = !this.isImageMode;
   }
 }
--- modal/scripts/main.js
+++ modal/scripts/main.js
@@ -2,11 +2,15 @@ import CaptchaManager from "./captcha-manager";
 
 function main() {
   const manager = new CaptchaManager();
+  manager.nextQuestion();
   let numImagesSelected = 0;
   
   const submitButton = document.getElementById("submit");
   const toggleLink = document.getElementById("toggle");
   const imageContainers = document.getElementsByClassName("image-container");
+  const imageSection = document.getElementById("images");
+  const audioSection = document.getElementById("audio");
+  const audioTextInput = document.getElementById("audio-text");
   
   submitButton.addEventListener("click", () => {
     // Clear image selections.
@@ -16,15 +20,33 @@ function main() {
       const circle = ic.children[1];
       _deselect(img, circle);
     }
+    // Clear the input field.
+    audioTextInput.innerHTML = "";
     // Disable the button.
     submitButton.disabled = true;
     manager.nextQuestion();
+    // Check if the question type switch link can now be shown.
     if (manager.canSwithQuestionTypes()) {
       toggleLink.style.visibility = "visible";
     }
   });
   toggleLink.addEventListener("click", () => {
     manager.switchQuestionTypes();
+    // Check if the question type switch link should now be hidden.
+    if (!manager.canSwithQuestionTypes()) {
+      toggleLink.style.visibility = "hidden";
+    }
+    if (imageSection.style.display === "none") {
+      // Switch to image questions.
+      imageSection.style.display = "block";
+      audioSection.style.display = "none";
+      toggleLink.innerHTML = "I am visually impaired.";
+    } else {
+      // Switch to audio questions.
+      imageSection.style.display = "none";
+      audioSection.style.display = "block";
+      toggleLink.innerHTML = "I am hearing impaired.";
+    }
   });
   for (const ic of imageContainers) {
     ic.addEventListener("click", () => {
@@ -46,6 +68,36 @@ function main() {
       }
     });
   }
+  audioTextInput.addEventListener("copy", e => {
+    const copyText = "\u25AF".repeat(10);
+    e.clipboardData.setData("text/plain", copyText);
+    e.preventDefault();
+  });
+  
+  audioTextInput.addEventListener("paste", e => {
+    // Taken from https://developer.mozilla.org/en-US/docs/Web/Events/paste#JavaScript_2.
+    e.preventDefault();
+    e.stopPropagation();
+    
+    // Get the clipboard data
+    let paste = (e.clipboardData || window.clipboardData).getData('text');
+    
+    // Insert spaces between characters.
+    paste = paste.replace(/(.)/g, "$1 ");
+    paste = paste.substring(0, paste.length - 1);
+
+    // Find the cursor location or highlighted area
+    const selection = window.getSelection();
+
+    // Cancel the paste operation if the cursor or highlighted area isn't found
+    if (!selection.rangeCount) return false;
+    
+    selection.deleteFromDocument();
+
+    // Paste the modified clipboard content where it was intended to go
+    selection.getRangeAt(0).insertNode(document.createTextNode(paste));
+    selection.collapseToEnd();
+  });
 }
 
 function _deselect(img, circle) {
